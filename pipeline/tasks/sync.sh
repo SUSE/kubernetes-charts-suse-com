0fed0a72af305673bb8dc50c3c942b1d529191e6
Dimitris Karakasilis <jimmykarily@gmail.com>
2017-11-10 17:22:08 +0200

    Create the helm packages and update the index.yaml

--- pipeline/tasks/sync.sh
+++ pipeline/tasks/sync.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-echo "This is the bucket: "$BUCKET
+set -e
+
+mkdir out
+
+# Run `helm init` before using any helm commands.
+# https://github.com/kubernetes/helm/issues/1732#issuecomment-268093699
+helm init --client-only
+
+# Create all the helm packages
+pushd ci/stable > /dev/null
+for dir in *
+do
+  helm dep update $dir
+  helm package $dir -d ../../out/
+done
+popd > /dev/null
+
+# Get the old index yaml from S3
+aws s3 cp s3://$BUCKET/index.yaml index.yaml --region eu-central-1
+
+# Update the index
+helm repo index --url https://kubernetes-charts.suse.com/ --merge index.yaml out
+
+# Now sync everything on AWS S3.
+# IMPORTANT: When making changes to this script, make sure you use the --dryrun
+# flag on this command before you actually let it write stuff on S3.
+aws s3 sync out/ s3://$BUCKET/ --region eu-central-1
