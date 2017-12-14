e6cbd51e670fce7405c13c755b4bc7a5b0e98632
Aaron Lefkowitz <aaron.lefkowitz@suse.com>
2017-12-14 15:24:34 -0800

    Escape URLs for S3

--- pipeline/tasks/sync.sh
+++ pipeline/tasks/sync.sh
@@ -23,6 +23,10 @@ aws s3 cp s3://$BUCKET/index.yaml index.yaml --region eu-central-1
 # Update the index
 helm repo index --url https://kubernetes-charts.suse.com/ --merge index.yaml out
 
+# Ensure s3 urls are properly escaped
+cat out/index.yaml | awk '{ if (/^\s+- https/) { gsub(/\+/,"%2B"); print } else print }' > index.yaml.escaped
+mv index.yaml.escaped out/index.yaml
+
 # Now sync everything on AWS S3.
 # IMPORTANT: When making changes to this script, make sure you use the --dryrun
 # flag on this command before you actually let it write stuff on S3.
